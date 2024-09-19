4dc931f8155cd60937d7aecf65ba960eecec81de
灵弦 <3257884944@qq.com>
2024-09-19 14:10:23 +0800

    wip:逻辑迭代,仓储旧代码,尝试引入ts-pattern

---
+++ src/utils/zipNumArray.ts
@@ -0,0 +1,45 @@
+const to = (start: number, end: number): Array<number> => {
+    let i: Array<number> = []
+    let go = (start: number, end: number) => {
+        i.push(start)
+        if (start === end) return i
+        else go(start + 1, end)
+    }
+    go(start, end)
+    return i
+}
+
+// 转换数组到数学区间以压缩空间占用
+function arrayToInterval(list: Array<number>): string {
+    if (list == undefined || list.length === 0) {
+        return '[]'
+    } else if (list.length === 1) {
+        return JSON.stringify([
+            [list[0], list[0]]
+        ])
+    }
+    list = [...new Set(list)]
+    list.sort((a, b) => (a - b))
+    let collection = []
+    collection.push([list[0], list[0]])
+    let i = 0
+    for (let x = 1; x < list.length; x++) {
+        if (list[x] - list[x - 1] !== 1) {
+            collection[i][1] = list[x - 1]
+            i += 1
+            collection.push([list[x], list[x]])
+        }
+    }
+    collection[i][1] = list[list.length - 1]
+    return JSON.stringify(collection)
+}
+
+// 转换数学区间到数组
+function intervalToArray(str: string): Array<number> {
+    let list = JSON.parse(str || '[]') as Array<[number, number]>
+    let res: Array<number> = []
+    list.forEach(e => res.push(...to(e[0], e[1])))
+    return res
+}
+
+export { arrayToInterval, intervalToArray }
