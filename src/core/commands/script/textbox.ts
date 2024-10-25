25d87c5b5257b2f29a8530e918fcd5bd9dab80d7
灵弦 <3257884944@qq.com>
2024-10-25 23:05:41 +0800

    wip:尝试引入anime.js,更新tween命令，仓储旧代码

--- src/core/commands/script/textbox.ts
+++ src/core/commands/script/textbox.ts
@@ -1,6 +1,7 @@
 import { CommandRunFunction } from '@/core/Command'
-import { useSignal } from '@/utils/Reactive'
+import { STAR_MARKER } from '@/ui/Hoshizora/Game/TextBox'
 import { Y } from '@/utils/FPUtil'
+import { useSignal } from '@/utils/Reactive'
 
 // 跨幕环境变量name,无副作用
 // 存在依赖变量的"文字播放速度"
@@ -14,17 +15,16 @@ export const textView = useSignal('')
 
 const text: CommandRunFunction<{ text: string }> =
     ({ store, timer }) =>
-    ({ text }) => {
-        console.log(text)
-        return Y<string, Promise<void>>((rec) => (str) => {
+    ({ text }) =>
+        Y<string, Promise<void>>((rec) => (str) => {
             return Promise.resolve()
                 .then(() => textView((text) => text + str.substring(0, 1)))
                 .then(() => timer.delay(store.config.TextSpeed * 100))
                 .then(() => {
                     if (str.length >= 1) return rec(str.substring(1))
+                    else textView((text) => text + STAR_MARKER)
                 })
         })(text)
-    }
 
 export const Text = { beforeInit: () => textView(''), onActStart: () => textView(''), run: text }
 
