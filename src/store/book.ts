ea6d075e6f31f170c37e50bc9262e96a6e706cdd
灵弦 <ahzvenol@users.noreply.github.com>
2024-11-02 19:31:19 +0800

    fix:修复原始剧本数据被修改的问题

--- src/store/book.ts
+++ src/store/book.ts
@@ -1,4 +1,11 @@
+import { cloneDeep } from 'es-toolkit'
 import request from '@/utils/request'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const book = request('./static/book.json').then((res) => res.data as Array<Array<Record<string, any>>>)
+const book = request('./static/book.json').then((res) => res.data as Array<Array<Record<string, any>>>)
+
+const length = async () => (await book).length
+
+const row = (index: number) => book.then((res) => cloneDeep(res[index]))
+
+export default { row, length }
