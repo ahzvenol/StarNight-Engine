15a1c31c36696b85ef00628cd9112bdc2c4b05d7
灵弦 <3257884944@qq.com>
2024-11-23 16:45:52 +0800

    wip:开发中,保存中间代码

--- src/core/commands/script/wait.ts
+++ src/core/commands/script/wait.ts
@@ -1,17 +1,9 @@
-import { TimeoutController } from '@/utils/TimeoutController'
 import { Dynamic } from '../../flow'
+import { _wait } from './abstract/wait'
 
-// 初始化过程中什么都不做
-// wait引用系统sleep实现
 export const wait = Dynamic<{ duration: number }>(
     ({ timer }) =>
         function* ({ duration }) {
-            yield new Promise<void>((res) => {
-                const controller = new TimeoutController(res, duration)
-                timer.addResumeMethod(controller.start)
-                timer.addPauseMethod(controller.pause)
-                timer.addFinalizeMethod(controller.immediateExecution)
-                if (!timer.isPaused) controller.start()
-            })
+            yield _wait(timer)(duration)
         }
 )
