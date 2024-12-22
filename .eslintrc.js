abf62c720c42242b5e0142213f0f0923d854ba9e
灵弦 <3257884944@qq.com>
2024-12-22 22:17:06 +0800

    refactor:不再使用classList属性,而是使用clsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:solid/recommended',
-        'plugin:prettier/recommended'
+        'plugin:prettier/recommended',
+        'plugin:clsx/recommended'
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -21,6 +22,7 @@ module.exports = {
     rules: {
         'solid/no-destructure': 'off', // 允许solid组件参数解构
         '@typescript-eslint/consistent-type-imports': 'error', // 如果导入类型（type），将导入类型和导出其他对象分开写
-        '@typescript-eslint/no-for-in-array': 'error' // 禁止使用for in来进行数组访问
+        '@typescript-eslint/no-for-in-array': 'error', // 禁止使用for in来进行数组访问
+        'clsx/prefer-objects-over-logical': 'error' // 禁止在clsx()中使用逻辑表达式
     }
 }
