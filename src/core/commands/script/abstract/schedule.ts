341d04911ed72299ad5a29803b077cf8ae4951d7
灵弦 <ahzvenol@users.noreply.github.com>
2025-01-04 23:00:48 +0800

    refactor:分离副作用,更清晰的表示执行流程

--- src/core/commands/script/abstract/schedule.ts
+++ src/core/commands/script/abstract/schedule.ts
@@ -1,3 +1,4 @@
+import type { CommandOutput } from '@/core/types/Command'
 import type { ScheduledStandardResolvedCommand, StandardResolvedCommandFunction } from '@/core/types/Schedule'
 import { merge, omit } from 'es-toolkit'
 import { Schedule } from '@/core/types/Schedule'
@@ -6,19 +7,29 @@ import { splitEffect } from '@/core/utils/splitEffect'
 // 并行执行接收到全部命令,无论Flow的具体类型
 // 完成时间是传入的命令中所需时间最长的那个
 export const _par: Function1<Array<ScheduledStandardResolvedCommand>, StandardResolvedCommandFunction> =
-    (array) => () =>
-        Promise.all(array.map((e) => e.apply())).then((results) => results.reduce(merge, {}))
+    (array) => () => {
+        const effects = array.map((e) => splitEffect(e.apply))
+        for (const effect of effects) {
+            effect.execute()
+        }
+        return _result(effects.map((e) => e.result))
+    }
 
-// 串行执行接收到全部命令,无论Flow的具体类型,行为相当于await cmd1();await cmd2();await cmd3()
+// 串行执行接收到全部命令,无论Flow的具体类型
 // 完成时间是传入的所有命令所需执行时间的和
 export const _chain: Function1<Array<ScheduledStandardResolvedCommand>, StandardResolvedCommandFunction> =
-    (array) => () =>
-        array.reduce((pre, e) => pre.then((all) => e.apply().then((result) => merge(all, result))), Promise.resolve({}))
+    (array) => async () => {
+        const effects = array.map((e) => splitEffect(e.apply))
+        for (const effect of effects) {
+            await effect.execute()
+        }
+        return _result(effects.map((e) => e.result))
+    }
 
-// 识别Await标识的命令,行为相当于Promise.all(cmd1();await cmd2();cmd3())
+// 根据Flow的具体类型,对Await标识的命令阻塞等待，对Async的命令并行执行
 // 完成时间是最后一个Await命令执行完毕之后,还在执行的命令,剩余执行时间最长的那个
-export const _fork: Function1<Array<ScheduledStandardResolvedCommand>, StandardResolvedCommandFunction> = (array) => {
-    return async () => {
+export const _fork: Function1<Array<ScheduledStandardResolvedCommand>, StandardResolvedCommandFunction> =
+    (array) => async () => {
         const effects = array.map((e) => ({ ...omit(e, ['apply']), ...splitEffect(e.apply) }))
         for (const effect of effects) {
             if (effect.meta.schedule === Schedule.Await) {
@@ -27,6 +38,8 @@ export const _fork: Function1<Array<ScheduledStandardResolvedCommand>, StandardR
                 effect.execute()
             }
         }
-        return Promise.all(effects.map((e) => e.result)).then((results) => results.reduce(merge, {}))
+        return _result(effects.map((e) => e.result))
     }
-}
+
+export const _result: Function1<Array<Promise<CommandOutput>>, Promise<CommandOutput>> = (array) =>
+    Promise.all(array).then((results) => results.reduce(merge, {}))
