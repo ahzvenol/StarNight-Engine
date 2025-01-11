b9c0770049863fb6ee959174104af84d1626a551
灵弦 <3257884944@qq.com>
2025-01-11 22:46:34 +0800

    refactor:分离宏展开和命令调度

--- src/core/commands/macros.ts
+++ src/core/commands/macros.ts
@@ -6,9 +6,7 @@ import { tweenImage } from './hoshizora/tween'
 import { video } from './hoshizora/video'
 
 // 宏存在顺序依赖,目前只有say需要在audio上面
-// 由于运行时解析和预加载解析的方式不同,需要注意:
-// 运行时逐层应用宏,所以如果宏返回多层结构,子层级还会再次被应用宏
-// 预加载逐个应用宏,单个宏被应用过后就不会再次应用
+// 逐个应用宏,单个宏被应用过后就不会再次应用
 export const macros = [
     Macro.from('setBG', setBG),
     Macro.from('setS', setSprite),
