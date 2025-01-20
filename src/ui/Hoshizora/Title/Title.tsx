d66cc3e5be323703ba23909d2648d9101fab5b15
灵弦 <3257884944@qq.com>
2025-01-20 22:41:53 +0800

    feat:移动端更大的按钮

--- src/ui/Hoshizora/Title/Title.tsx
+++ src/ui/Hoshizora/Title/Title.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js'
 import clsx from 'clsx'
 import { router } from '@/router'
-import { Button, Clone, line, Variable } from '@/ui/Elements'
+import { Button, Clone, Variable } from '@/ui/Elements'
 import { restartGame } from '@/ui/Pages'
 import { Pages } from '@/ui/Type'
 import { log } from '@/utils/logger'
@@ -11,39 +11,45 @@ const Title: Component = () => {
     log.info('Title组件函数被调用')
     return (
         <div class={clsx('Page', styles.Title_container)}>
-            <Clone count={4}>
-                {(index) => (
-                    <Variable value={['01start', '02load', '03config', '04gallery'][index]}>
-                        {(imageId) => (
-                            <>
-                                <Button
-                                    class={`Title_button_${index}`}
-                                    style={{
-                                        width: `121px`,
-                                        height: `26px`,
-                                        right: `570px`,
-                                        bottom: `${55 + 41 * (4 - line(4)(index))}px`,
-                                        'background-image': `url('./static/Texture2D/title_${imageId}.webp')`
-                                    }}
-                                    onClick={() => {
-                                        if (index === 0) restartGame()
-                                        router.navigate([Pages.Game, Pages.Load, Pages.Config, Pages.Gallery][index])
-                                    }}
-                                />
-                                <style jsx>
-                                    {`
-                                        @media (hover: hover) {
-                                            .Title_button_${index}:hover {
-                                                background-image: url('./static/Texture2D/title_${imageId}_on.webp') !important;
+            <div class={styles.Title_button_container}>
+                <Clone count={4}>
+                    {(index) => (
+                        <Variable value={['01start', '02load', '03config', '04gallery'][index]}>
+                            {(imageId) => (
+                                <>
+                                    <Button
+                                        class={`Title_button_${index}`}
+                                        style={{
+                                            'background-image': `url('./static/Texture2D/title_${imageId}.webp')`
+                                        }}
+                                        onClick={() => {
+                                            if (index === 0) restartGame()
+                                            router.navigate(
+                                                [Pages.Game, Pages.Load, Pages.Config, Pages.Gallery][index]
+                                            )
+                                        }}
+                                    />
+                                    <style jsx>
+                                        {`
+                                            @media (hover: hover) {
+                                                .Title_button_${index}:hover {
+                                                    background-image: url('./static/Texture2D/title_${imageId}_on.webp') !important;
+                                                }
                                             }
-                                        }
-                                    `}
-                                </style>
-                            </>
-                        )}
-                    </Variable>
-                )}
-            </Clone>
+                                            @media (hover: none) {
+                                                .Title_button_${index} {
+                                                    transform: scale(1.35) translate(0, ${index * 5}px);
+                                                    transform-origin: top;
+                                                }
+                                            }
+                                        `}
+                                    </style>
+                                </>
+                            )}
+                        </Variable>
+                    )}
+                </Clone>
+            </div>
         </div>
     )
 }
