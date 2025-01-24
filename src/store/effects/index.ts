6b556f9677176105df3d0671f6c408f5925dbbfd
灵弦 <3257884944@qq.com>
2025-01-24 22:46:19 +0800

    feat:记录成就

--- src/store/effects/index.ts
+++ src/store/effects/index.ts
@@ -1,4 +1,5 @@
 // 为store对应变量分派功能
+import { effect as effect5 } from './achievement'
 import { effect as effect2 } from './fullscreen'
 import { effect as effect4 } from './latestversion'
 import { effect as effect3 } from './title'
@@ -8,3 +9,4 @@ effect1()
 effect2()
 effect3()
 effect4()
+effect5()
