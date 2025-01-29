fa8c7ca09f449474b5027b3a5732884d28637573
灵弦 <3257884944@qq.com>
2025-01-29 23:59:19 +0800

    wip:开发更新

--- src/store/effects/fullscreen.ts
+++ src/store/effects/fullscreen.ts
@@ -1,16 +1,19 @@
 import { createEffect } from 'solid-js'
+import { isNative } from '@/utils/checkEnv'
 import { log } from '@/utils/logger'
 import { storePromise } from '../store'
 
 export const effect = () =>
     storePromise.then((store) => {
-        // 并没有办法把用户使用F11等按键触发的全屏同步到变量,因为浏览器不会返回相关信息
-        createEffect(() => {
-            log.info('全屏模式: ' + store.config.fullscreen())
-            if (store.config.fullscreen()) {
-                document.documentElement.requestFullscreen()
-            } else {
-                if (document.fullscreenElement !== null) document.exitFullscreen()
-            }
-        })
+        if (!isNative()) {
+            // 并没有办法把用户使用F11等按键触发的全屏同步到变量,因为浏览器不会返回相关信息
+            createEffect(() => {
+                log.info('全屏模式: ' + store.config.fullscreen())
+                if (store.config.fullscreen()) {
+                    document.documentElement.requestFullscreen()
+                } else {
+                    if (document.fullscreenElement !== null) document.exitFullscreen()
+                }
+            })
+        }
     })
