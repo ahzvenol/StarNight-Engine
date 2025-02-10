cb12d72b259475251a39875695f70412aeed4953
灵弦 <3257884944@qq.com>
2025-02-10 20:10:54 +0800

    fix:修改根对象后Effect触发异常

--- src/utils/solid/useReactive.ts
+++ src/utils/solid/useReactive.ts
@@ -5,5 +5,6 @@ import { createMutable } from 'solid-js/store'
 
 // 通过solid的store对象实现响应式,通过micro-reactive实现任意解构和函数风格
 export function useReactive<T extends StoreNode>(value: T): Reactive<T> {
-    return useMicroReactive(createMutable(value))
+    // 需要让value不是根对象以保证value(newValue)时Effect工作正常
+    return useMicroReactive(createMutable([value]))[0]
 }
