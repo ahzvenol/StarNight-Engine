7416c8d81c4c3c305de924d1b8c17c132e9acbab
灵弦 <3257884944@qq.com>
2025-02-14 23:56:48 +0800

    chore:整理代码

---
+++ src/core/commands/script/blind.ts
@@ -0,0 +1,19 @@
+import { ActScope, Dynamic } from '@/core/decorator'
+import { useActScopeSignal } from '@/core/utils/useScopeSignal'
+import { wait } from './wait'
+
+export const displayBlindView = useActScopeSignal(false)
+
+export const blind = ActScope(
+    Dynamic(
+        'Normal.Await',
+        (context) =>
+            function* () {
+                displayBlindView(true)
+                yield wait.apply(context)({
+                    duration: 300
+                }) as unknown as Promise<void>
+                displayBlindView(false)
+            }
+    )
+)
