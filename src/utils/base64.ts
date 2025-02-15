e4beb4a4e1c0efe1e702b89ff4b2cf750d8f27f8
灵弦 <3257884944@qq.com>
2025-02-15 14:37:33 +0800

    chore:整理代码

--- src/utils/base64.ts
+++
@@ -1,3 +0,0 @@
-export const jsonToBase64 = (str: string) => window.btoa(unescape(encodeURIComponent(str)))
-
-export const base64ToJson = (base64: string) => decodeURIComponent(escape(window.atob(base64)))
