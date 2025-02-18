fad90c5d4696043253115d220d2efb31e05129da
灵弦 <3257884944@qq.com>
2025-02-18 17:11:56 +0800

    fix:schedule标志运算异常

--- src/core/commands/script/system/schedule.ts
+++ src/core/commands/script/system/schedule.ts
@@ -8,13 +8,12 @@ import { _chain, _fork, _par } from '../abstract/schedule'
 // meta.key只用于调试
 
 export const Fork: ScheduledHighLevelCommand = {
-    meta: { schedule: Schedule.Async },
     apply: (context) => (rows) =>
         _fork(
             rows.flatMap((row) => {
                 const cmd = commands()[row.key]
-                if (!isUndefined(cmd.meta.exclude) && context.state in cmd.meta.exclude) return []
-                const schedule = cmd.meta.schedule || row.await ? Schedule.Await : Schedule.Async
+                if (!isUndefined(cmd.meta?.exclude) && context.state in cmd.meta.exclude) return []
+                const schedule = cmd.meta?.schedule || (row.await ? Schedule.Await : Schedule.Async)
                 const apply = () => cmd.apply(context)(row.args as any)
                 return [{ meta: { key: row.key, schedule }, apply }]
             })
@@ -22,12 +21,11 @@ export const Fork: ScheduledHighLevelCommand = {
 }
 
 export const Par: ScheduledHighLevelCommand = {
-    meta: { schedule: Schedule.Async },
     apply: (context) => (rows) =>
         _par(
             rows.flatMap((row) => {
                 const cmd = commands()[row.key]
-                if (!isUndefined(cmd.meta.exclude) && context.state in cmd.meta.exclude) return []
+                if (!isUndefined(cmd.meta?.exclude) && context.state in cmd.meta.exclude) return []
                 const schedule = Schedule.Async
                 const apply = () => cmd.apply(context)(row.args as any)
                 return [{ meta: { key: row.key, schedule }, apply }]
@@ -36,12 +34,11 @@ export const Par: ScheduledHighLevelCommand = {
 }
 
 export const Chain: ScheduledHighLevelCommand = {
-    meta: { schedule: Schedule.Async },
     apply: (context) => (rows) =>
         _chain(
             rows.flatMap((row) => {
                 const cmd = commands()[row.key]
-                if (!isUndefined(cmd.meta.exclude) && context.state in cmd.meta.exclude) return []
+                if (!isUndefined(cmd.meta?.exclude) && context.state in cmd.meta.exclude) return []
                 const schedule = Schedule.Await
                 const apply = () => cmd.apply(context)(row.args as any)
                 return [{ meta: { key: row.key, schedule }, apply }]
