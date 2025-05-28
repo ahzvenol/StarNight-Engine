c19b8b95856ee9aaf762578248c1e0005884c7de
灵弦 <3257884944@qq.com>
2025-05-28 22:54:38 +0800

    chore:暂时不再需要解析器

--- packages/parser/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { GameBookRaw } from '@starnight/core'
-
-export declare function compile(source: string): GameBookRaw
--- packages/parser/src/main/util/FPUtil.scala
+++
@@ -1,5 +0,0 @@
-package util
-
-object FPUtil {
-  extension [A](value: A) inline infix def |>[B](f: Function[A, B]): B = f(value)
-}
