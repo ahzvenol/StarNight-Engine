ee1a3072c08832c0fcea08345abb309467641550
灵弦 <3257884944@qq.com>
2025-06-04 01:36:20 +0800

    feat:富文本支持

--- apps/webgal/src/core/scripts/base/say.ts
+++ apps/webgal/src/core/scripts/base/say.ts
@@ -1,6 +1,9 @@
 import type { Reactive } from '@starnight/core'
 import { ActScope, Dynamic, NonBlocking, StarNight } from '@starnight/core'
-import { System } from '.'
+import { gsap } from 'gsap'
+import { SplitText } from 'gsap/SplitText'
+
+gsap.registerPlugin(SplitText)
 
 declare module '@starnight/core' {
     interface GameUIInternalData {
@@ -44,36 +47,22 @@ StarNight.ActEvents.start.subscribe(({ current, ui }) => {
     ui.text.innerHTML = ''
 })
 
-function f(node: HTMLElement, speed: number, timeline: gsap.core.Timeline) {
-    for (const child of node.children) {
-        if (child.nodeType === Node.TEXT_NODE) {
-            const text = node.textContent!
-            const frag = document.createDocumentFragment()
-            for (const char of text) {
-                const span = document.createElement('span')
-                span.textContent = char
-                frag.appendChild(span)
-                timeline.fromTo(span, { opacity: 0 }, { opacity: 1, duration: speed })
-            }
-            child.parentNode!.replaceChild(frag, node)
-        } else if (child instanceof HTMLElement) {
-            f(child, speed, timeline)
-        }
-    }
-}
+const compareElementOrder = (a: Node, b: Node) => (a.compareDocumentPosition(b) & 2 ? 1 : a.compareDocumentPosition(b) & 4 ? -1 : 0)
 
 export const text = ActScope(
     Dynamic<string | HTMLElement>(
-        (context) =>
+        ({ current, config, ui }) =>
             function* (arg0) {
-                const nodes = arg0 instanceof HTMLElement ? [...arg0.childNodes] : [arg0]
-                context.current.text(arg0 instanceof HTMLElement ? arg0.outerHTML : arg0)
-                context.ui.text.append(nodes.shift()!)
-                console.log(context.config.textspeed(), nodes)
-                while (nodes.length >= 1) {
-                    yield System.wait(context.config.textspeed())(context)
-                    context.ui.text.append(nodes.shift()!)
-                }
+                const container = document.createElement('div')
+                container.append(arg0)
+                ui.text.append(container)
+                current.text(ui.text.innerHTML)
+                const rubys = container.querySelectorAll('ruby')
+                const split = SplitText.create(ui.text, { type: 'chars', aria: 'hidden', smartWrap: true })
+                const nodes = split.chars.concat(Array.from(rubys)).sort(compareElementOrder)
+                const speed = config.textspeed() / 1000
+                const timeline = gsap.from(nodes, { opacity: 0, duration: nodes.length * speed, ease: 'sine.out', stagger: speed })
+                yield new Promise((res) => timeline.eventCallback('onComplete', res))
             }
     )
 )
