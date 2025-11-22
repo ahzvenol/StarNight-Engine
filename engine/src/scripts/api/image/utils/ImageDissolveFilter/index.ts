dbc44475ff8665f5174c35ee8d80e65b0eaa5225
灵弦 <3257884944@qq.com>
2025-11-22 22:27:45 +0800

    style: explicit-member-accessibility

--- engine/src/scripts/api/image/utils/ImageDissolveFilter/index.ts
+++ engine/src/scripts/api/image/utils/ImageDissolveFilter/index.ts
@@ -6,11 +6,11 @@ import fragment from './image_dissolve_300.frag'
 export type ImageDissolveFilterOptions = { progress?: number, ramplen?: number, reverse?: boolean }
 
 export class ImageDissolveFilter extends Filter {
-    constructor(uTexture: Texture, options: ImageDissolveFilterOptions = {}) {
+    public constructor(uTexture: Texture, options: ImageDissolveFilterOptions = {}) {
         const { progress = 0, ramplen = 8, reverse = false } = options
         super(vertex, fragment, { uTexture, progress, ramplen, reverse })
     }
 
-    get progress() { return this.uniforms.progress }
-    set progress(v: number) { this.uniforms.progress = v }
+    public get progress() { return this.uniforms.progress }
+    public set progress(v: number) { this.uniforms.progress = v }
 }
